daa64442b47031d310fd85af072510326ac80a0a
Matthew Nguyen <matthewnguyen0814@gmail.com>
2021-12-02 17:46:42 -0600

    Footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => (
+ 
+    <div className="footer">
+     <div className="email">
+         <li><a href="mailto:matthewnguyen0814@gmail.com" title="matthewnguyen0814@gmail.com">Contact me</a></li>
+     </div>
+ </div>
+
+);
+
+export default Footer;
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
+import Footer from './Footer';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
@@ -27,6 +28,7 @@ export default function PortfolioContainer() {
     <div>
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {renderPage()}
+      <Footer/>
     </div>
   );
 }
